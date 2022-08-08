8c8fee7ab621c65121e5d6ca20ffb282a8279d9d
aleedca <acerdas1701@estudiantec.cr>
2022-08-07 21:45:13 -0600

    greatest number method

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -13,23 +13,41 @@
     public void GenerateArray(){
         for(int index = 0; index < arraySize; index++){
             int number = random.Next(1,100);
-            array[index] = number; // add random number to the array
+            array[index] = number; // add random number to array
         }
     }
 
-    public void GreatestNumber(){
+    public int GreatestNumber(){
+        int greatestNumber = 0;
 
+        for(int index = 0; index < arraySize; index++){
+            int next = index + 1;
+
+            if(next < arraySize){
+                if(array[index] >= array[next]){
+                    greatestNumber = array[index];
+                }
+                else{
+                    greatestNumber = array[next];
+                }
+            }
+        }
+        return greatestNumber;
     }
 
     public void PrintArray(){
+        Console.Write("Array: [");
         for(int index = 0; index < arraySize; index++){
-            Console.WriteLine($"Array[{index}]: {array[index]}");
+            Console.Write(array[index]);
+            if(index < arraySize-1) Console.Write(",");
         }
+        Console.WriteLine("]");
     }
     
     private static void Main(string[] args){
         Program p = new Program();
         p.GenerateArray();
-        p.printArray();
+        p.PrintArray();
+        Console.Write(p.GreatestNumber());
     }
 }
