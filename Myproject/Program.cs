15c65d6362dd824ddc434fd666060dc1126d085f
aleedca <acerdas1701@estudiantec.cr>
2022-08-07 21:16:58 -0600

    print array methodand few changes

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -1,22 +1,35 @@
 ﻿class Program
 {
-    public void GenerateArray(int arraySize){
-        Random random = new Random();
-        int[] array = new int[arraySize+1];
+    int arraySize;
+    int[] array;
+    Random random;
 
+    public Program(){
+        arraySize = 10;
+        array = new int[arraySize+1];
+        random = new Random();
+    }
+
+    public void GenerateArray(){
         for(int index = 0; index < arraySize; index++){
             int number = random.Next(1,100);
             array[index] = number; // add random number to the array
-            // Console.WriteLine($"Array[{index}]: {number}");
         }
     }
 
     public void GreatestNumber(){
 
     }
+
+    public void PrintArray(){
+        for(int index = 0; index < arraySize; index++){
+            Console.WriteLine($"Array[{index}]: {array[index]}");
+        }
+    }
     
     private static void Main(string[] args){
         Program p = new Program();
-        p.GenerateArray(10);
+        p.GenerateArray();
+        p.printArray();
     }
 }
