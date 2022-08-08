37264be3dcf88eadf4f83e5606699e37dec41cae
aleedca <acerdas1701@estudiantec.cr>
2022-08-07 20:07:02 -0600

    generate random number

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -1,7 +1,16 @@
 ﻿class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    public void GenerateArray(){
+        Random random = new Random();
+        int arraySize = 10; // assign the size of the array
+        for(int index = 0; index < arraySize; index++){
+            int number = random.Next(1,100);
+            Console.WriteLine($"Array[{index}]: {number}");
+        }
+    }
+    
+    private static void Main(string[] args){
+        Program p = new Program();
+        p.GenerateArray();
     }
 }
