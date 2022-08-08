998a3bd68c554b58be2a8d9944cbfc0f4fdf966a
aleedca <acerdas1701@estudiantec.cr>
2022-08-07 20:52:04 -0600

    method modification

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -2,11 +2,17 @@
 {
     public void GenerateArray(int arraySize){
         Random random = new Random();
-        
+        int[] array = new int[arraySize+1];
+
         for(int index = 0; index < arraySize; index++){
             int number = random.Next(1,100);
-            Console.WriteLine($"Array[{index}]: {number}");
+            array[index] = number; // add random number to the array
+            // Console.WriteLine($"Array[{index}]: {number}");
         }
+    }
+
+    public void GreatestNumber(){
+
     }
     
     private static void Main(string[] args){
