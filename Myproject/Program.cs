bcd29b364d492ff72b9eb5945c49528647049d3d
aleedca <acerdas1701@estudiantec.cr>
2022-08-07 20:12:37 -0600

    method modification

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -1,8 +1,8 @@
 ﻿class Program
 {
-    public void GenerateArray(){
+    public void GenerateArray(int arraySize){
         Random random = new Random();
-        int arraySize = 10; // assign the size of the array
+        
         for(int index = 0; index < arraySize; index++){
             int number = random.Next(1,100);
             Console.WriteLine($"Array[{index}]: {number}");
@@ -11,6 +11,6 @@
     
     private static void Main(string[] args){
         Program p = new Program();
-        p.GenerateArray();
+        p.GenerateArray(10);
     }
 }
