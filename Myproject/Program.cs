e160a94be2b1dbe8db773e43a2b4899a9a8f963a
aleedca <acerdas1701@estudiantec.cr>
2022-08-08 18:33:55 -0600

    counter added

--- Myproject/Program.cs
+++ Myproject/Program.cs
@@ -3,28 +3,36 @@
     int arraySize;
     int[] array;
     Random random;
+    int counter;
 
     public Program(){
         arraySize = 10;
+        counter = 0;
         array = new int[arraySize+1];
         random = new Random();
     }
 
     public void GenerateArray(){
         for(int index = 0; index < arraySize; index++){
-            int number = random.Next(1,5);
+            int number = random.Next(1,10);
             array[index] = number; // add random number to array
         }
     }
 
     public int GreatestNumber(){
         int greatestNumber = array[0];
+        counter++;
+        counter += (2*arraySize) + 2;
         for(int index = 0; index < arraySize; index++){
             int next = index + 1;
+            counter++;
+            counter += 2;
             if((next < arraySize) && (greatestNumber < array[next])){
                 greatestNumber = array[next];
+                counter++;
             }
         }
+        counter ++;
         return greatestNumber;
     }
 
@@ -41,6 +49,7 @@
         Program p = new Program();
         p.GenerateArray();
         p.PrintArray();
-        Console.Write(p.GreatestNumber());
+        Console.WriteLine(p.GreatestNumber());
+        Console.WriteLine($"Time complexity: {p.counter}");
     }
 }
